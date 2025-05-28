7ca7839786669f5934bb2a27ec1cf1a16bc4e9f9
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2025-05-28 14:53:47 +0530

    pass error to globalErrorHandler

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,24 +1,24 @@
 import { authService } from "../services/authService";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export const authController = {
-    async login(req: Request, res: Response) {
+    async login(req: Request, res: Response, next: NextFunction) {
         try {
             const { email, password } = req.body;
             const { user, token } = await authService.login(email, password);
             res.status(200).json({ user, token });
         } catch (error) {
-            res.status(500).json({ message: "Internal server error" });
+            next(error);
         }
     },
     
-    async signup(req: Request, res: Response) {
+    async signup(req: Request, res: Response, next: NextFunction) {
         try {
             const { email, password } = req.body;
             const { user, token } = await authService.signup(email, password);
             res.status(200).json({ user, token });
         } catch (error) {
-            res.status(500).json({ message: "Internal server error" });
+            next(error);
         }
     }
 }
